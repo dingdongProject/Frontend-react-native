dfc632c7b16726789afe9c9880e1e53955dedf82
2016026480 <junslim11@gmail.com>
2020-11-06 22:37:27 +0900

    fix: onboarding

--- src/Screens/Onboarding/index.tsx
+++ src/Screens/Onboarding/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
-import {Alert, Image} from 'react-native';
+import {Alert} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {StackNavigationProp} from '@react-navigation/stack';
 import Styled from 'styled-components/native';
@@ -10,7 +10,9 @@ interface Props {
     navigation : NavigationProp;
 }
 
-
+const Image = Styled.Image`
+    height: 500px
+`;
 
 
 const onboard = ({navigation} : Props) =>{
@@ -22,29 +24,30 @@ useEffect(() => {
 return(
 <Onboarding
     nextLabel = '다음'
-    skipLabel = '로그인 화면으로'
+    skipLabel = '건너뛰기'
     onDone={()=>navigation.navigate('Login')}
     onSkip={()=>navigation.navigate('Login')}
+    bottomBarHighlight={false}
     transitionAnimationDuration = {2500}
     pages={[
         {
             backgroundColor : '#fff',
-            image : <Image source={require('~/Assets/Images/OT.png')}/>,
+            image : <Image source={require('~/Assets/Images/OT.png')} />,
             title : '임',
-            subtitle : 'swiper',
+            subtitle: 'Done with React Native Onboarding Swiper',
             
         },
         {
             backgroundColor : '#cecece',
             image : <Image source={require('~/Assets/Images/OT.png')}/>,
             title : '준',
-            subtitle : 'swiper'
+            subtitle: 'Done with React Native Onboarding Swiper',
         },
         {
             backgroundColor : '#000',
             image : <Image source={require('~/Assets/Images/OT.png')}/>,
             title : '섭',
-            subtitle : 'swiper',
+            subtitle: 'Done with React Native Onboarding Swiper',
             
         },
     ]}/>
